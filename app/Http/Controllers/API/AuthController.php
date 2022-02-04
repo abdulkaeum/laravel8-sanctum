439a61656db5123638a03c5444a788ae0e82b98d
abdulkaeum <abdul.kaeum@hotmail.com>
2022-02-04 10:44:39 +0000

    create the authentication controller for register, login and logout

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+
+class AuthController extends Controller
+{
+    /**
+     * validate/create the user and their token
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function create(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users', 'email')],
+            'password' => ['required', 'string', 'min:8']
+        ]);
+
+        if($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'data' => $user,
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ]);
+    }
+
+    /**
+     * attempt to login the user and create a token and store it in the personal_access_tokens table
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Unauthorised'], 401);
+        }
+
+        $user = User::where('email', $request->email)->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Welcome' . $user->name,
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ]);
+    }
+
+    /**
+     * log the user out and remove their token
+     * @return string[]
+     */
+    public function destroy()
+    {
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'You have been logged out and your token has been deleted'
+        ];
+    }
+
+
+}
