8810113228d815d470449f419edf3c22d56b20f5
abdulkaeum <abdul.kaeum@hotmail.com>
2022-02-04 14:56:49 +0000

    create API resource to transorm the json returned

---
+++ app/Http/Resources/GalaxyResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GalaxyResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'light_years' => $this->light_years . ' ly',
+            'name' => $this->name,
+            'about' => $this->about
+        ];
+    }
+
+    public function with($request)
+    {
+        return [
+            'info' => 'Top 5 Galaxies That Look Awesome',
+            'src' => 'https://steemit.com/science/@darthnava/top-5-galaxies-that-look-awesome'
+        ];
+    }
+}
